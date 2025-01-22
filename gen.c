65a1a19670eb13c89f74aed72c53222d304e621d
Tape Dispenser <59711006+Tape-Dispenser@users.noreply.github.com>
2025-01-21 23:00:48 -0700

    another header change
    what can i say i like reformatting

--- gen.c
+++ gen.c
@@ -1,7 +1,6 @@
 /*
  * Passphrase Generator: Command line tool to generate cryptographically secure passphrases
- * Copyright (C) 2025, Ada Gramiak
- *   Contact: <adadispenser@gmail.com>
+ * Copyright (C) 2025, Ada Gramiak, <adadispenser@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
